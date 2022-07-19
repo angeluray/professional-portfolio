452834f6227df1c49d05a5c73e0211ac9ad5ea84
angeluray <aeucampos@gmail.com>
2022-07-19 18:11:37 -0400

    Fix linter erros in css file and html file

--- js/index.js
+++ js/index.js
@@ -1,12 +1,12 @@
-const menuHamburguer = document.querySelector(".burguer-menu");
-const myNav = document.querySelector(".menu-sections");
+const menuHamburguer = document.querySelector('.burguer-menu');
+const myNav = document.querySelector('.menu-sections');
 
-menuHamburguer.addEventListener("click", () => {
-    menuHamburguer.classList.toggle("active");
-    myNav.classList.toggle("active");
-})
+menuHamburguer.addEventListener('click', () => {
+  menuHamburguer.classList.toggle('active');
+  myNav.classList.toggle('active');
+});
 
-document.querySelectorAll(".link-nav").forEach(n => n.addEventListener("click", () => {
-    menuHamburguer.classList.remove("active");
-    myNav.classList.remove("active");
-}))
+document.querySelectorAll('.link-nav').forEach((n) => n.addEventListener('click', () => {
+  menuHamburguer.classList.remove('active');
+  myNav.classList.remove('active');
+}));
