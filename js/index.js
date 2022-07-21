777c62ad9cb6e04c820635c7b393bf13271bc3d7
angeluray <aeucampos@gmail.com>
2022-07-21 10:23:13 -0400

    Add cards content dynamically to HTML using JS

--- js/index.js
+++ js/index.js
@@ -10,3 +10,84 @@ document.querySelectorAll('.link-nav').forEach((n) => n.addEventListener('click'
   menuHamburguer.classList.remove('active');
   myNav.classList.remove('active');
 }));
+
+const myWorkContainer = document.querySelector('.my-work');
+
+const myCardsContent = [
+
+  {
+    myCardId: 'card-1',
+    workImage: 'images/imageplaceholder.png',
+    workTitle: 'Multi-Post Stories',
+    workDescription: 'A daily selection of privately personalized reads; no accounts or sign-ups required. has been the industry\'s standard dummy text ever since the 1500s, when an unknown printer took a standard dummy text.',
+    workTools: ['css', 'html', 'boostrap', 'ruby'],
+    seeLive: 'https://www.youtube.com/watch?v=4fQeaM62mOY',
+    seeSource: 'https://www.youtube.com/watch?v=4fQeaM62mOY',
+    myMbId: 'btn-1',
+
+  },
+
+  {
+    myCardId: 'card-2',
+    workImage: 'images/imageplaceholder.png',
+    workTitle: 'Multi-Post Stories',
+    workDescription: 'A daily selection of privately personalized reads; no accounts or sign-ups required. has been the industry\'s standard dummy text ever since the 1500s, when an unknown printer took a standard dummy text.',
+    workTools: ['css', 'html', 'boostrap', 'ruby'],
+    seeLive: 'https://www.youtube.com/watch?v=4fQeaM62mOY',
+    seeSource: 'https://www.youtube.com/watch?v=4fQeaM62mOY',
+    myMbId: 'btn-2',
+
+  },
+
+  {
+    myCardId: 'card-3',
+    workImage: 'images/imageplaceholder.png',
+    workTitle: 'Multi-Post Stories',
+    workDescription: 'A daily selection of privately personalized reads; no accounts or sign-ups required. has been the industry\'s standard dummy text ever since the 1500s, when an unknown printer took a standard dummy text.',
+    workTools: ['css', 'html', 'boostrap', 'ruby'],
+    seeLive: 'https://www.youtube.com/watch?v=4fQeaM62mOY',
+    seeSource: 'https://www.youtube.com/watch?v=4fQeaM62mOY',
+    myMbId: 'btn-3',
+
+  },
+
+  {
+    myCardId: 'card-4',
+    workImage: 'images/imageplaceholder.png',
+    workTitle: 'Multi-Post Stories',
+    workDescription: 'A daily selection of privately personalized reads; no accounts or sign-ups required. has been the industry\'s standard dummy text ever since the 1500s, when an unknown printer took a standard dummy text.',
+    workTools: ['css', 'html', 'boostrap', 'ruby'],
+    seeLive: 'https://www.youtube.com/watch?v=4fQeaM62mOY',
+    seeSource: 'https://www.youtube.com/watch?v=4fQeaM62mOY',
+    myMbId: 'btn-4',
+
+  },
+
+];
+
+const dynamiContent = () => {
+  let myVarCont = '';
+
+  for (let i = 0; i < myCardsContent.length; i += 1) {
+    const myCardCon = `<div class="cards" id="${myCardsContent[i].myCardId}">
+
+       <div class="image-container">
+          <img src="${myCardsContent[i].workImage}" alt="First project visualization" class="my-project-image">
+       </div>
+
+       <div class="my-flex-card">
+          <h2 class="my-work-title">${myCardsContent[i].workTitle}</h2>
+          <p class="my-work-description">${myCardsContent[i].workDescription}</p>
+           <ul class="my-work-tools">
+              <li class="li-css">${myCardsContent[i].workTools[0]}</li>
+              <li>${myCardsContent[i].workTools[1]}</li>
+              <li>${myCardsContent[i].workTools[2]}</li>
+              <li>${myCardsContent[i].workTools[3]}</li>
+           </ul>
+          <button class="visualize-button actv" id="${myCardsContent[i].myMbId}"type="button">See Project</button>
+        </div></div>`;
+    myVarCont += myCardCon;
+  }
+  myWorkContainer.insertAdjacentHTML('beforeend', myVarCont);
+};
+dynamiContent();
