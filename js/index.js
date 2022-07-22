1d3c8f595c94791706d5a2cba030041390c82342
angeluray <aeucampos@gmail.com>
2022-07-21 20:45:29 -0400

    Add cards dynamically and and modal popup

--- js/index.js
+++ js/index.js
@@ -18,48 +18,56 @@ const myCardsContent = [
   {
     myCardId: 'card-1',
     workImage: 'images/imageplaceholder.png',
+    modalImage: 'images/test-image.png',
+    modalImgClose: 'images/cancel.png',
     workTitle: 'Multi-Post Stories',
     workDescription: 'A daily selection of privately personalized reads; no accounts or sign-ups required. has been the industry\'s standard dummy text ever since the 1500s, when an unknown printer took a standard dummy text.',
     workTools: ['css', 'html', 'boostrap', 'ruby'],
     seeLive: 'https://www.youtube.com/watch?v=4fQeaM62mOY',
     seeSource: 'https://www.youtube.com/watch?v=4fQeaM62mOY',
-    myMbId: 'btn-1',
+    myMbId: 'btnId1',
 
   },
 
   {
     myCardId: 'card-2',
     workImage: 'images/imageplaceholder.png',
+    modalImage: 'images/test-image.png',
+    modalImgClose: 'images/cancel.png',
     workTitle: 'Multi-Post Stories',
     workDescription: 'A daily selection of privately personalized reads; no accounts or sign-ups required. has been the industry\'s standard dummy text ever since the 1500s, when an unknown printer took a standard dummy text.',
     workTools: ['css', 'html', 'boostrap', 'ruby'],
     seeLive: 'https://www.youtube.com/watch?v=4fQeaM62mOY',
     seeSource: 'https://www.youtube.com/watch?v=4fQeaM62mOY',
-    myMbId: 'btn-2',
+    myMbId: 'btnId2',
 
   },
 
   {
     myCardId: 'card-3',
     workImage: 'images/imageplaceholder.png',
+    modalImage: 'images/snp-image.png',
+    modalImgClose: 'images/cancel.png',
     workTitle: 'Multi-Post Stories',
-    workDescription: 'A daily selection of privately personalized reads; no accounts or sign-ups required. has been the industry\'s standard dummy text ever since the 1500s, when an unknown printer took a standard dummy text.',
+    workDescription: 'A dailyyy selection of privately personalized reads; no accounts or sign-ups required. has been the industry\'s standard dummy text ever since the 1500s, when an unknown printer took a standard dummy text.',
     workTools: ['css', 'html', 'boostrap', 'ruby'],
     seeLive: 'https://www.youtube.com/watch?v=4fQeaM62mOY',
     seeSource: 'https://www.youtube.com/watch?v=4fQeaM62mOY',
-    myMbId: 'btn-3',
+    myMbId: 'btnId3',
 
   },
 
   {
     myCardId: 'card-4',
     workImage: 'images/imageplaceholder.png',
+    modalImage: 'images/test-image.png',
+    modalImgClose: 'images/cancel.png',
     workTitle: 'Multi-Post Stories',
     workDescription: 'A daily selection of privately personalized reads; no accounts or sign-ups required. has been the industry\'s standard dummy text ever since the 1500s, when an unknown printer took a standard dummy text.',
     workTools: ['css', 'html', 'boostrap', 'ruby'],
     seeLive: 'https://www.youtube.com/watch?v=4fQeaM62mOY',
     seeSource: 'https://www.youtube.com/watch?v=4fQeaM62mOY',
-    myMbId: 'btn-4',
+    myMbId: 'btnId4',
 
   },
 
@@ -69,7 +77,7 @@ const dynamiContent = () => {
   let myVarCont = '';
 
   for (let i = 0; i < myCardsContent.length; i += 1) {
-    const myCardCon = `<div class="cards" id="${myCardsContent[i].myCardId}">
+    const myCardCon = ` <div class="cards" id="${myCardsContent[i].myCardId}">
 
        <div class="image-container">
           <img src="${myCardsContent[i].workImage}" alt="First project visualization" class="my-project-image">
@@ -84,10 +92,117 @@ const dynamiContent = () => {
               <li>${myCardsContent[i].workTools[2]}</li>
               <li>${myCardsContent[i].workTools[3]}</li>
            </ul>
-          <button class="visualize-button actv" id="${myCardsContent[i].myMbId}"type="button">See Project</button>
+          <button class="visualize-button actv" id="${myCardsContent[i].myMbId}"type="button" >See Project</button>
         </div></div>`;
     myVarCont += myCardCon;
   }
   myWorkContainer.insertAdjacentHTML('beforeend', myVarCont);
 };
 dynamiContent();
+
+// ...........MODAL POPUP........................................
+
+const btnId1 = document.getElementById('btnId1');
+const btnId2 = document.getElementById('btnId2');
+const btnId3 = document.getElementById('btnId3');
+const btnId4 = document.getElementById('btnId4');
+
+let i = 0;
+
+function display() {
+  const popbody = document.querySelector('body');
+  const windowSection = document.createElement('section');
+  const divContainer = document.createElement('div');
+  const sectionDiv = document.createElement('div');
+  const windowHeader = document.createElement('header');
+  const windowTitle = document.createElement('h1');
+  const ClosImg = document.createElement('img');
+  const divImgContainer = document.createElement('div');
+  const windowImg = document.createElement('img');
+  const popupParagraph = document.createElement('p');
+  const ul = document.createElement('ul');
+  const ulLi1 = document.createElement('li');
+  const ulLi2 = document.createElement('li');
+  const ulLi3 = document.createElement('li');
+  const divButton = document.createElement('div');
+  const popBtn1 = document.createElement('button');
+  const popBtn2 = document.createElement('button');
+
+  popbody.className = 'popbody';
+  windowSection.className = 'popupsection';
+  divContainer.className = 'popwindow-Container';
+  sectionDiv.className = 'popup-div';
+  windowHeader.className = 'popup-H';
+  windowTitle.className = 'windowTitle';
+  ClosImg.className = 'cancel';
+  divImgContainer.className = 'div-Img-Container';
+  windowImg.className = 'window-div-Img ';
+  popupParagraph.className = 'divP';
+  ul.className = 'listMain';
+  ulLi1.className = 'list1';
+  ulLi2.className = 'list2';
+  ulLi3.className = 'list3';
+  divButton.className = 'button-div';
+  popBtn1.className = 'button1';
+  popBtn2.className = 'button2';
+
+  popbody.appendChild(windowSection);
+  windowSection.appendChild(divContainer);
+  divContainer.appendChild(sectionDiv);
+  sectionDiv.appendChild(windowHeader);
+  windowHeader.appendChild(windowTitle);
+  windowHeader.appendChild(ClosImg);
+  sectionDiv.appendChild(divImgContainer);
+  divImgContainer.appendChild(windowImg);
+  sectionDiv.appendChild(popupParagraph);
+  sectionDiv.appendChild(ul);
+  ul.appendChild(ulLi1);
+  ul.appendChild(ulLi2);
+  ul.appendChild(ulLi3);
+  sectionDiv.appendChild(divButton);
+  divButton.appendChild(popBtn1);
+  divButton.appendChild(popBtn2);
+
+  document.querySelector('.windowTitle').textContent = myCardsContent[i].workTitle;
+  document.querySelector('.cancel').src = 'images/cancel.png';
+  document.querySelector('.window-div-Img').src = myCardsContent[i].modalImage;
+  document.querySelector('.divP').textContent = myCardsContent[i].workDescription;
+  document.querySelector('.list1').textContent = myCardsContent[i].workTools[0];
+  document.querySelector('.list2').textContent = myCardsContent[i].workTools[1];
+  document.querySelector('.list3').textContent = myCardsContent[i].workTools[2];
+  document.querySelector('.list3').textContent = myCardsContent[i].workTools[3];
+  document.querySelector('.button1').textContent = 'See Live';
+  document.querySelector('.button2').textContent = 'See Source';
+
+  ClosImg.addEventListener('click', () => {
+    windowSection.remove();
+  });
+
+  popBtn1.addEventListener('click', () => {
+    window.open(myCardsContent[i].btnLive);
+  });
+
+  popBtn2.addEventListener('click', () => {
+    window.open(myCardsContent[i].btnSource);
+  });
+}
+
+btnId1.addEventListener('click', () => {
+  i = 0;
+  display();
+});
+
+btnId2.addEventListener('click', () => {
+  i = 1;
+  display();
+});
+
+btnId3.addEventListener('click', () => {
+  i = 2;
+  display();
+});
+
+btnId4.addEventListener('click', () => {
+  i = 3;
+  display();
+});
